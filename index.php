cd02b3c5faa91aed650fe16f74ac93f78078253f
lai2-01 <lai2-01@itismeucci.com>
2022-03-21 11:37:40 +0100

    iniziato

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+  $method = $_SERVER['REQUEST_METHOD'];
+  if ($method == 'POST'){
+    
+
+  } elseif ($method == 'GET'){
+    
+
+  } elseif ($method == 'PUT'){
+      
+
+  } elseif ($method == '\DELETE'){
+     
+
+  }
+?>
