d0961c4f111f887dbf9c97839dd2b08835663fd8
Monkele Kongpi <75998171+michelecoppi@users.noreply.github.com>
2022-04-06 13:37:24 +0200

    get funzionante

--- index.php
+++ index.php
@@ -1,12 +1,41 @@
 <?php
+
   require("connessionedatabase.php");
+  $array;
+  $size=20;
+  $page=0;
+  $elementi;
+  $query="select count(id) as tot from employees";
+    if ($result = $mysqli->query($query)) {
+      while ($row = $result->fetch_assoc()) {
+        $elementi=$row["tot"];
+      }
+    }
+$paginetot=ceil($elementi/$size);
 
   $method = $_SERVER['REQUEST_METHOD'];
   if ($method == 'POST'){
     
 
   } elseif ($method == 'GET'){
-    
+     
+    $query="select * from employees order by id limit ".$page.",".$size;
+    if ($result = $mysqli->query($query)) {
+      while ($row = $result->fetch_assoc()) {
+        $array[]=$row;
+      }
+    }
+
+$pagine=array(
+"size" => $size,
+"total_Elements" => $elementi,
+"TotalPages" => $paginetot,
+"number" => $page
+);
+
+$array[]=["pages" => $pagine];
+$data=json_encode( $array);
+echo $data;
 
   } elseif ($method == 'PUT'){
       
